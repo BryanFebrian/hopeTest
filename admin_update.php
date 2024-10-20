36cc72b7d5e8972c0e7932aa703ff6afcff37966
BryanFebrian <bryanfebrian951@gmaiil.com>
2024-10-20 08:51:38 +0700

    gmau edit semua, maunya one by one

--- admin_update.php
+++ admin_update.php
@@ -6,28 +6,34 @@ $id = $_GET['edit'];
 
 if(isset($_POST['update_product'])){
 
-   $product_name = $_POST['product_name'];
-   $product_price = $_POST['product_price'];
-   $product_image = $_FILES['product_image']['name'];
-   $product_image_tmp_name = $_FILES['product_image']['tmp_name'];
-   $product_image_folder = 'uploaded_img/'.$product_image;
+   // Mendapatkan nilai saat ini dari database
+   $select = mysqli_query($conn, "SELECT * FROM products WHERE id = '$id'");
+   $row = mysqli_fetch_assoc($select);
 
-   if(empty($product_name) || empty($product_price) || empty($product_image)){
-      $message[] = 'please fill out all!';    
-   }else{
+   // Menangkap nilai input, atau menggunakan nilai yang sudah ada jika kosong
+   $product_name = !empty($_POST['product_name']) ? $_POST['product_name'] : $row['name'];
+   $product_price = !empty($_POST['product_price']) ? $_POST['product_price'] : $row['price'];
 
-      $update_data = "UPDATE products SET name='$product_name', price='$product_price', image='$product_image'  WHERE id = '$id'";
-      $upload = mysqli_query($conn, $update_data);
-
-      if($upload){
-         move_uploaded_file($product_image_tmp_name, $product_image_folder);
-         header('location:admin_page.php');
-      }else{
-         $$message[] = 'please fill out all!'; 
-      }
-
+   // Jika file image diupload, maka perbarui, jika tidak biarkan yang lama
+   if (!empty($_FILES['product_image']['name'])) {
+      $product_image = $_FILES['product_image']['name'];
+      $product_image_tmp_name = $_FILES['product_image']['tmp_name'];
+      $product_image_folder = 'uploaded_img/'.$product_image;
+      move_uploaded_file($product_image_tmp_name, $product_image_folder); // Pindahkan file yang baru
+   } else {
+      $product_image = $row['image']; // Gunakan image lama jika tidak ada yang diupload
    }
-};
+
+   // Perbarui produk dengan data yang baru atau yang lama (jika tidak diubah)
+   $update_data = "UPDATE products SET name='$product_name', price='$product_price', image='$product_image' WHERE id = '$id'";
+   $upload = mysqli_query($conn, $update_data);
+
+   if($upload){
+      header('location:admin_page.php');
+   } else {
+      $message[] = 'Error occurred while updating product!';
+   }
+}
 
 ?>
 
@@ -51,7 +57,6 @@ if(isset($_POST['update_product'])){
 
 <div class="container">
 
-
 <div class="admin-product-form-container centered">
 
    <?php
@@ -62,19 +67,15 @@ if(isset($_POST['update_product'])){
    ?>
    
    <form action="" method="post" enctype="multipart/form-data">
-      <h3 class="title">update the product</h3>
-      <input type="text" class="box" name="product_name" value="<?php echo $row['name']; ?>" placeholder="enter the product name">
-      <input type="number" min="0" class="box" name="product_price" value="<?php echo $row['price']; ?>" placeholder="enter the product price">
-      <input type="file" class="box" name="product_image"  accept="image/png, image/jpeg, image/jpg">
-      <input type="submit" value="update product" name="update_product" class="btn">
-      <a href="admin_page.php" class="btn">go back!</a>
+      <h3 class="title">Update the Product</h3>
+      <input type="text" class="box" name="product_name" value="<?php echo $row['name']; ?>" placeholder="Enter the product name">
+      <input type="number" min="0" class="box" name="product_price" value="<?php echo $row['price']; ?>" placeholder="Enter the product price">
+      <input type="file" class="box" name="product_image" accept="image/png, image/jpeg, image/jpg">
+      <input type="submit" value="Update Product" name="update_product" class="btn">
+      <a href="admin_page.php" class="btn">Go back!</a>
    </form>
-   
-
 
    <?php }; ?>
-
-   
 
 </div>
 
